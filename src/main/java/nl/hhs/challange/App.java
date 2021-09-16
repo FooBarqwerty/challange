ed0ec64502538da123dcc2d7a8758c788f6d3441
Lance_vdB <18009395@student.hhs.nl>
2021-09-16 13:11:15 +0200

    Initial commit

---
+++ src/main/java/nl/hhs/challange/App.java
@@ -0,0 +1,13 @@
+package nl.hhs.challange;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
